0556f56de8837cf793c4f13e57e12d4bf1b6e77c
lezginskidelight <ferhad.ov.ilqar0@gmail.com>
2026-01-17 14:46:37 +0400

    Added vote average

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,14 @@ import Image from "next/image"
 export default function Card({
   poster_path,
   title,
+  vote_average,
 }: {
   poster_path: string
   title: string
+  vote_average: number
 }) {
   return (
-    <div className="gap-3 flex flex-col border border-light max-w-[200px]">
+    <div className="gap-3 flex flex-col border border-light max-w-[200px] relative">
       <Image
         src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
         alt="poster"
@@ -16,6 +18,7 @@ export default function Card({
         height={200}
       />
       <div className="h-full">{title}</div>
+      <div className="absolute top-2 end-2 px-2 bg-red-500 border border-light rounded">{vote_average.toFixed(1)}</div>
     </div>
   )
 }
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,6 +9,7 @@ export default function HomePage({ data }: { data: MovieArray }) {
           key={movie.id}
           poster_path={movie.poster_path}
           title={movie.title}
+          vote_average={movie.vote_average}
         />
       ))}
     </main>
