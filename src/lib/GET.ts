09335e8286b40e27c504e9d109fe4178a049891a
lezginskidelight <ferhad.ov.ilqar0@gmail.com>
2026-01-17 14:33:34 +0400

    Fetch functions

--- src/lib/GET.ts
+++ src/lib/GET.ts
@@ -1,6 +1,6 @@
 import { getRegion } from "./getRegion"
 
-export async function getDataFromTMDB() {
+export async function getTopRatedLocal() {
   const region = await getRegion()
   console.log(region)
   const baseURL = process.env.NEXT_PUBLIC_API_URL
@@ -8,7 +8,37 @@ export async function getDataFromTMDB() {
   if (!baseURL || !API_KEY) {
     throw new Error("Error")
   }
-  const url = `${baseURL}/movie/popular?api_key=${API_KEY}`
+  const url = `${baseURL}/movie/top_rated?api_key=${API_KEY}&region=${region}`
+  const res = await fetch(url)
+  if (!res.ok) {
+    throw new Error("Error ok")
+  }
+  const data = await res.json()
+  return data.results
+}
+
+export async function getNowPlaying() {
+  const baseURL = process.env.NEXT_PUBLIC_API_URL
+  const API_KEY = process.env.TMDB_API_KEY
+  if (!baseURL || !API_KEY) {
+    throw new Error("Error")
+  }
+  const url = `${baseURL}/movie/now_playing?api_key=${API_KEY}`
+  const res = await fetch(url)
+  if (!res.ok) {
+    throw new Error("Error ok")
+  }
+  const data = await res.json()
+  return data.results
+}
+
+export async function getPopular() {
+  const baseURL = process.env.NEXT_PUBLIC_API_URL
+  const API_KEY = process.env.TMDB_API_KEY
+  if (!baseURL || !API_KEY) {
+    throw new Error("Error")
+  }
+  const url = `${baseURL}/movie/popular?api_key=${API_KEY}&page=2`
   const res = await fetch(url)
   if (!res.ok) {
     throw new Error("Error ok")
